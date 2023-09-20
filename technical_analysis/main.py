569d0e0c416ad3ec33928b3b8fdac9521afd994c
Rodrigo <Martinez>
2023-09-19 19:14:13 -0600

    signal generation for 3 indicators done

--- technical_analysis/main.py
+++ technical_analysis/main.py
@@ -1,6 +1,7 @@
 from itertools import combinations
+import ta
 
-a = [1,2,3,4,5,6]
+a = [ta.trend.ichimoku_a, ta.trend.SMAIndicator, ta.trend.MACD, ta.momentum.RSIIndicator, ta.momentum.ROCIndicator, ta.momentum.awesome_oscillator]
 
 for j in range(len(a)):
     print([i for i in combinations(a, j)])
